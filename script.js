34ec63fcc953ccb18576577cb4d428151d9ce4d8
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2023-07-04 01:01:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,36 @@
+
 let scrapeEmails = document.getElementById("scrapeEmails");
 let list =  document.getElementById("text-list")
 let namePerson =  document.getElementById("name-input")
 
 
 
-   //  Handler to recieve emails from content scripting
-   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let gettingpost =  request.gettingpost;
-    let gettingName =  request.gettingName;
+document.addEventListener("DOMContentLoaded", async function() {
+   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  
-       list.innerText = gettingpost;
-       namePerson.value = gettingName;
-    
+   chrome.scripting.executeScript({
+       target: { tabId: tab.id },
+       func: replyforthepost,
+   });
 
-})
+   function replyforthepost() {
+       let commentInnerbox = document.getElementsByClassName("ql-editor");
+       let getPostReply = document.createElement('div');
+       getPostReply.innerHTML = `©`;
+       getPostReply.className = "engagebtn";
 
+       Array.from(commentInnerbox).forEach((comment) => {
+           comment.addEventListener("click", function(event) {
+               let emogibox = event.currentTarget.parentNode.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("mlA")[0];
+               emogibox.prepend(getPostReply);
 
-// button's click event listener
-scrapeEmails.addEventListener("click" , async function(){
-  
-    // get current active tab
-    let [tab] = await  chrome.tabs.query({
-        active:true, currentWindow:true
-      });
+               getPostReply.addEventListener('click', function(event) {
+                   let takethisincomment = event.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("feed-shared-update-v2__description-wrapper")[0].innerText;
+                   event.currentTarget.parentElement.parentElement.getElementsByClassName("ql-editor")[0].getElementsByTagName("p")[0].innerText = takethisincomment;
+                   console.log(takethisincomment);
+               });
+           })
+       })
+   }
+});
 
-
-    //   excute script to parse email on page
-     chrome.scripting.executeScript({
-        target: {tabId : tab.id},
-        func: scrapingemailfromweb,
-     })      
-
-     function scrapingemailfromweb(){
-
-       
-    
-  const gettingpost   = document.getElementsByClassName("update-components-text ")[0].getElementsByTagName("span")[0].getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerText
-
-
-  const gettingName =  document.getElementsByClassName("update-components-actor__name")[0].getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerText;
-
-  //  alert([gettingName , gettingpost])
-  // console.log(gettingpost)
-
-
-
-
-    chrome.runtime.sendMessage({gettingpost, gettingName})
-      
-     }
-
-})
